32aada0bf2bf7aa8059166679906dc44d470d858
Huigyun Jeong <topone03@gmail.com>
2025-08-23 01:58:44 +0900

    chore: setup project structure

--- MoJoLEG/App/MoJoLEGApp.swift
+++ MoJoLEG/App/MoJoLEGApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct MoJoLEGApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            DevView()
         }
     }
 }
---
+++ MoJoLEG/Presentation/View/Dev/DevView.swift
@@ -0,0 +1,18 @@
+//
+//  DevView.swift
+//  MoJoLEG
+//
+//  Created by 정희균 on 8/23/25.
+//
+
+import SwiftUI
+
+struct DevView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    DevView()
+}
