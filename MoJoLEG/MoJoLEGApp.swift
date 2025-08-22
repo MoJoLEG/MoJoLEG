c0037a93a9453f826963cd0dd57fee8c43e55610
Huigyun Jeong <topone03@gmail.com>
2025-08-23 01:35:06 +0900

    chore: setup project

---
+++ MoJoLEG/MoJoLEGApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoJoLEGApp.swift
+//  MoJoLEG
+//
+//  Created by 정희균 on 8/23/25.
+//
+
+import SwiftUI
+
+@main
+struct MoJoLEGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
